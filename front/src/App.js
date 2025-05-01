ff69c5dec2199f2d9a5de85abc19bfdd93055752
iamyeongg <syz101@soongsil.ac.kr>
2025-05-01 11:31:56 +0900

    [feat] tailwind 환경설정

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
