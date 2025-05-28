47bec804a3e8ac76093b965607ef3e320bd19fba
iamyeongg <syz101@soongsil.ac.kr>
2025-05-28 11:25:41 +0900

    [feat] main

--- front/src/pages/Mainpage.jsx
+++ front/src/pages/Mainpage.jsx
@@ -8,33 +8,37 @@ function Mainpage() {
   const handleClick = () => {
     navigate("/OptionPage");
   };
+
   return (
     <div>
-      <div className="flex flex-wrap justify-between items-center bg-white px-40 py-10">
-        <div className="w-1/2 flex ml-8 flex-col max-w-md space-y-6 gap-7 ">
-          <h1 className="text-4xl font-bold text-black">
-            모두를 위한 여행, 당신만의 <br /> 완벽한 계획을 세워보세요.
+      <div className="flex flex-col md:flex-row justify-between items-center bg-white px-6 py-10 md:px-20 lg:px-40">
+        <div className="w-full md:w-1/2 flex flex-col space-y-6 md:space-y-7">
+          <h1 className="text-3xl md:text-4xl font-bold text-black">
+            모두를 위한 여행, 당신만의 <br className="hidden md:block" /> 완벽한 계획을 세워보세요.
           </h1>
-          <p className="text-lg text-gray-600">
+          <p className="text-base md:text-lg text-gray-600">
             저희는 모두가 편안하게 여행을 즐길 수 있도록 돕습니다.
-            <br />
+            <br className="hidden md:block" />
             장애 유형별 맞춤 정보, 접근성 좋은 숙소와 명소 추천까지 당신만의
             여행 스타일에 맞는 플랜을 쉽게 만들고, 자유롭게 떠나보세요.
           </p>
-          <p className="text-lg font-semibold text-gray-800">
+          <p className="text-base md:text-lg font-semibold text-gray-800">
             이제, 장애는 여행의 걸림돌이 아닙니다.
-            <br />
+            <br className="hidden md:block" />
             당신의 이야기를 채울 새로운 여행을 시작해보세요.
           </p>
-          <button className="pt-20" onClick={handleClick}>
-            <Mainbutton className="w-96" />
+          <button className="pt-10" onClick={handleClick}>
+            <div className="w-96">
+              <Mainbutton className="w-full h-auto" />
+            </div>
           </button>
         </div>
-        <div className="w-1/2 relative w-[704px] h-[768px] overflow-hidden">
-          <Mainimage className="absolute inset-0 w-full h-full object-cover" />
+        <div className="w-full md:w-1/2 relative h-[300px] md:h-[768px] md:w-[704px] overflow-hidden mt-10 md:mt-0">
+          <Mainimage className="absolute inset-0 w-full h-full object-contain" />
         </div>
       </div>
     </div>
   );
 }
+
 export default Mainpage;
